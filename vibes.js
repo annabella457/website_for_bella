d5df00b77ee9f882c7a9ec25725bad12fa0dc35b
annabella457 <annabellabokla@gmail.com>
2025-10-20 17:27:47 -0500

    added w3 schools code for making a slideshow with transitions

--- vibes.js
+++ vibes.js
@@ -1,11 +1,39 @@
+// This script manages the simple image slideshow with navigation controls.
+// It allows users to navigate through images using next/previous buttons and thumbnail dots.
+// credit: W3Schools (modified)
+// https://www.w3schools.com/howto/howto_js_slideshow.asp
 
+let slideIndex = 1;
 
-window.onscroll = function() {scrollFunction()};
+window.onload = function() {
+  showSlides(slideIndex);
+};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("h1").style.fontSize = "30px";
-  } else {
-    document.getElementById("h1").style.fontSize = "90px";
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
